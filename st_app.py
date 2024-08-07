3fb50cddedbc81dc62d1ebcdd21b86dbb7170824
Prashantkhobragade <prashantkhobragade011@gmail.com>
2024-08-07 12:09:41 +0530

    updated

--- st_app.py
+++ st_app.py
@@ -37,27 +37,30 @@ if uploaded_file is not None:
         # Define agents
         openapi_analyst_agent = Agent(
             role="OpenAPI Specification Analyst",
-            goal="Analyze and interpret OpenAPI specifications data to create a comprehensive understanding of the API structure",
-            backstory="You are a seasoned API architect with 20 years of experience in designing and documenting APIs.",
+            goal="Analyze and interpret OpenAPI specifications data {data} to create a comprehensive understanding of the API structure",
+            backstory="You are a seasoned API architect with 20 years of experience in designing and documenting APIs. Your expertise lies in quickly grasping complex API structures and translating technical specifications into clear, actionable insights. You've worked on hundreds of API projects across various industries, making you an unparalleled expert in API analysis.",
             verbose=True,
-            llm=llm,
+            llm = llm,
             allow_delegation=False
         )
 
         user_request_interpreter_agent = Agent(
             role="User Request Interpreter, API Matcher",
-            goal="Interpret user request, identify the method, parameters, and match them to appropriate API endpoints based on the OpenAPI specification.",
-            backstory="With a background in both natural language processing and API integration, you excel at translating user requests into structured data.",
+            goal="""Interpret user request {request}, identify the method, parameters, and match them to appropriate API endpoints based on 
+                    the OpenAPI specification. Ensure the agent parses the user's request accurately to identify the correct API endpoint and parameters.""",
+            backstory="With a background in both natural language processing and API integration, you excel at translating user requests into structured data. Your 10 years of experience in building conversational AI systems that interact with complex APIs have made you an expert in generating precise JSON outputs for various API interactions.",
+            #tools = [unified_endpoint_connector],
             verbose=True,
             llm=llm,
+            #max_iter = 12,
             allow_delegation=False
         )
 
         api_call_agent = Agent(
-            role="API Integration Specialist",
-            goal="To efficiently and accurately interact with various API endpoints and handle errors gracefully.",
-            backstory="As a seasoned API Integration Specialist, I have extensive experience in working with diverse APIs across multiple domains.",
-            tools=[unified_endpoint_connector],
+            role = "API Integration Specialist",
+            goal = """To efficiently and accurately interact with various API endpoints. and Ensure that the agent itself is handling errors gracefully and returning clear messages""",
+            backstory = "As a seasoned API Integration Specialist, I have extensive experience in working with diverse APIs across multiple domains. My expertise lies in understanding API structures, authentication methods, and data formats. I was created to bridge the gap between complex API systems and user requirements, making data access and manipulation a breeze for users of all technical levels.",
+            tools = [unified_endpoint_connector],
             verbose=True,
             llm=llm,
             allow_delegation=False
@@ -65,21 +68,35 @@ if uploaded_file is not None:
 
         # Define tasks
         analyze_openapi_task = Task(
-            description="Thoroughly analyze the provided OpenAPI JSON data.",
-            expected_output="A comprehensive breakdown of the API structure, including endpoints, methods, parameters, and response structures.",
-            agent=openapi_analyst_agent
+            description="Thoroughly analyze the provided OpenAPI JSON data. Understand all endpoints, their purposes, parameters, request bodies, and response structures. Create a detailed mental model of the API's capabilities, limitations, and overall structure.",
+            expected_output="""A comprehensive breakdown of the API structure, including:
+                1. List of all available endpoints with their HTTP methods
+                2. Purpose of each endpoint
+                3. Required and optional parameters for each endpoint
+                4. Request body structures where applicable
+                5. Response structures and status codes
+            """,
+            agent = openapi_analyst_agent
         )
 
         interpret_user_request_task = Task(
-            description="Interpret user request and determine which API endpoint(s) would be most appropriate to fulfill their needs.",
-            expected_output="A clear interpretation of the user's intention, identified API endpoint(s), and required parameters or request body data.",
-            agent=user_request_interpreter_agent
+            description="Listen to user request {request} Identify the Method, params and determine which API endpoint(s) would be most appropriate to fulfill their needs. Translate natural language requests into specific API calls, taking into account the API structure provided by the OpenAPI Analyst.",
+            expected_output="""For each user request:
+                1. A clear interpretation of the user's intention
+                2. Identification of the Method(GET, POST, DELETE) to fulfill the request. 
+                3. Identification of the most appropriate API endpoint(s) to fulfill the request
+                4. Any required parameters or request body data needed for the API call
+                5. A JSON output which contains information extracted from above Foure points.
+                """,
+            agent = user_request_interpreter_agent
         )
 
         api_call_task = Task(
-            description="Analyze the output of previous Agents and Tasks, create a dynamic URL based on params and appropriate endpoint, then make a call to API.",
-            expected_output="The result of the API call, including success message or error details if applicable.",
-            agent=api_call_agent
+            description = """analyze the output of previous Agents and Tasks, create a dynamic url based on params and appropriate endpoint. 
+                    Then, make a call to API. 
+                    Ensure that errors are handled gracefully and return clear messages like if url is not found then return error: 404""",
+            expected_output="If the operation is successful, return a success message along with a JSON output containing only the request result. Otherwise, return an error message",
+            agent = api_call_agent
         )
 
         # Create crew
